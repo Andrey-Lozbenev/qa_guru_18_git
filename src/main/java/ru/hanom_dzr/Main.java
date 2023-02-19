c1dfb204bce420c18b1cd3404647cdb30b1c0300
Andrey <hanom_dzr@mail.ru>
2023-02-19 10:10:48 +0300

    test conflict

--- src/main/java/ru/hanom_dzr/Main.java
+++ src/main/java/ru/hanom_dzr/Main.java
@@ -2,6 +2,6 @@ package ru.hanom_dzr;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Andrey!");
+        System.out.println("Hello Andrey L!");
     }
 }
